eb349c2eff7491114a6758879f8b7cdf4c6d9a9d
Jay Cribas <feverjay@gmail.com>
2017-05-09 11:29:54 -0700

    Connect to Twitter API

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import { router } from './routes/routes'
 
 const server = express()
 
 server.use(bodyParser.urlencoded({extended:true}))
 server.set('view engine', 'pug')
 server.use(express.static(__dirname + '/public'))
+server.use(router)
 
 server.get('/', (req, res) => {
   res.render('index')
